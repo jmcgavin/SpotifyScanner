b41e8c8c487be4c573920810c9382c49d997f21d
jmcgavin <jmcgavin13@gmail.com>
2020-01-11 10:44:36 -0500

    import custom dropdown

--- src/components/ss-dropdown.js
+++ src/components/ss-dropdown.js
@@ -1,13 +1,19 @@
 import { LitElement, html, css } from 'lit-element'
 import { GlobalStyles } from '../styles/global-styles'
 
-import './ss-button'
-
-export class SSDropdown extends LitElement {
+/**
+ * `ss-dropdown` renders a dropdown menu containing a vertical list of items.
+ *
+ * @memberOf components
+ * @extends LitElement
+ * @property {String} title The selected menu item
+ * @property {Boolean} opened Whether or not the menu is opened
+ */
+class SSDropdown extends LitElement {
   static get properties () {
     return {
-      _label: { type: String },
-      _opened: { type: Boolean, reflect: true, attribute: 'opened' }
+      title: { type: String },
+      opened: { type: Boolean, reflect: true }
     }
   }
 
@@ -16,82 +22,209 @@ export class SSDropdown extends LitElement {
       GlobalStyles,
       css`
         :host {
-          height: var(--app-button-height);
-          display: block;
-          position: relative;
+          user-select: none;
         }
-        label {
+        :host([opened]) .trigger {
+          pointer-events: none;
+        }
+        :host([opened]) .overlay {
+          opacity: 1;
+        }
+        .overlay {
           position: absolute;
-          transform: translateY(-100%);
-          font-size: 12px;
-          color: var(--app-light-text);
-        }
-        #overlay {
+          min-width: 100%;
+          max-width: fit-content;
+          top: 0;
+          z-index: 1;
+          box-shadow: none;
           opacity: 0;
-          position: absolute;
-          top: calc(var(--app-button-height) + 8px);
-          left: 0;
-          background-color: var(--app-light-background);
-          transition: opacity 150ms ease;
+          transition: opacity .2s ease;
+        }
+        .menuContent {
+          display: flex;
+          flex-direction: column;
+          padding: 14px 16px;
           border-radius: 3px;
-          padding: 8px 0;
+          background-color: green;
+        }
+        .menuContent[hidden] {
+          display: none;
+        }
+        .menuContent::after {
+          width: 16px;
+          height: 16px;
+          display: block;
+          transform: rotate(45deg);
+          box-shadow: none;
+          background-color: blue;
+          content: '';
+          position: absolute;
+          top: -8px;
+          left: 24px;
           z-index: -1;
         }
-        :host([opened]) #overlay {
-          opacity: 1;
-          z-index: 1;
-        }
-        ul {
-          list-style: none;
-          margin: 0;
-          padding: 0;
-        }
-        ul li {
-          white-space: nowrap;
-          padding: 8px;
+        slot[name="item"]::slotted(button) {
+          font-size: 14px;
+          font-weight: bold;
+          height: 40px;
+          font-family: Roboto, sans-serif;
+          color: pink;
+          border: none;
+          outline: none;
+          background-color: transparent;
           cursor: pointer;
+          border-radius: 3px;
+          margin: 2px 0;
+          padding: 8px;
+          display: flex;
+          align-items: center;
+          transition: background-color .2s ease;
         }
-        ul li:hover {
-          background-color: var(--app-hover);
+
+        slot[name="item"]::slotted(button:not([selected]):hover),
+        slot[name="item"]::slotted(button:not([selected]):focus) {
+          background-color: orange;
         }
-        ul li[selected] {
-          background-color: var(--app-active);
+        slot[name="item"]::slotted([selected]) {
+          background-color: var(--app-primary-color);
+          color: tomato;
+        }
+        slot[name="item"]::slotted([selected]:focus) {
+          background-color: salmon;
         }
       `
     ]
   }
 
+  createRenderRoot () {
+    return this.attachShadow({ mode: 'open', delegatesFocus: true })
+  }
+
+  get _overlayElement () {
+    return this.shadowRoot.querySelector('.overlay')
+  }
+
+  get _triggerElement () {
+    return this.shadowRoot.querySelector('.trigger')
+  }
+
+  get _overlayContentElement () {
+    return this.shadowRoot.querySelector('.overlay div')
+  }
+
   constructor () {
     super()
-    this._label = 'Select a playlist'
-    this._opened = false
+    this._boundCaptureBodyClick = this._captureBodyClick.bind(this)
+    this._boundKeyDown = this._onKeyDown.bind(this)
   }
 
   render () {
     return html`
-      <label for="playlistSelector">Playlist</label>
-      <ss-button
-        id="playlistSelector"
-        .label=${this._label}
-        .trailingIcon=${'arrow_drop_down'}
-        @click=${this._toggleDropdown}>
-      </ss-button>
-      
-      <div id="overlay">
-        <ul>
-          <li>Item 1 is a very long list item and it will break everything</li>
-          <li>Item 2</li>
-          <li>Item 3</li>
-          <li>Item 4</li>
-          <li>Item 5</li>
-        </ul>
+      <div class="trigger" @click="${this.open}">
+        <slot></slot>
+      </div>
+      <div class="overlay" @click="${e => e.target.blur()}">
+        <div class="menuContent" ?hidden="${!this.opened}">
+          <slot name="item"></slot>
+        </div>
       </div>
     `
   }
 
-  _toggleDropdown () {
-    this._opened ? this._opened = false : this._opened = true
+  shouldUpdate (changedProperties) {
+    if (changedProperties.has('opened')) {
+      this.dispatchEvent(new CustomEvent(
+        'opened-changed',
+        {
+          detail: this.opened
+        }
+      ))
+    }
+    return super.shouldUpdate(changedProperties)
+  }
+
+  firstUpdated () {
+    // Close the overlay when the icon button is focused
+    this.addEventListener('focus', this.close)
+    this.addEventListener('blur', this._onBlur, true)
+    this._overlayElement.addEventListener('keydown', this._boundKeyDown)
+    this._overlayElement.addEventListener('click', this._boundClick)
+  }
+
+  updated (changedProperties) {
+    if (changedProperties.has('opened') && this.opened) {
+      this.querySelector('button, [href], [tabindex]:not([tabindex="-1"])').focus()
+    }
+  }
+
+  open () {
+    this.opened = true
+    document.documentElement.addEventListener('click', this._boundCaptureBodyClick, true)
+  }
+
+  close () {
+    document.documentElement.removeEventListener('click', this._boundCaptureBodyClick, true)
+    this.opened = false
+  }
+
+  _onBlur (e) {
+    if (e.composedPath && e.composedPath().indexOf(document.body) === -1) return
+    this.close()
+  }
+
+  /**
+   * @param {Event} e
+   * @private
+   */
+  _captureBodyClick (e) {
+    if (e.composedPath && e.composedPath().indexOf(this._overlayElement) !== -1) return
+    this.close()
+  }
+
+  /**
+   * On keydown of the overlay
+   * @param {Event} e
+   * @private
+   */
+  _onKeyDown (e) {
+    let focusTarget = null
+    const tabItems = Array.from(
+      e.target.parentElement
+        .querySelectorAll('button[slot="item"], [href][slot="item"], [tabindex]:not([tabindex="-1"])[slot="item"]')
+    )
+    const index = tabItems.findIndex(node => node === e.target)
+    switch (e.key) {
+      case 'Tab':
+      case 'Escape':
+        this.close()
+        break
+      case 'ArrowUp':
+        if (tabItems[index - 1]) {
+          // previous item
+          focusTarget = tabItems[index - 1]
+        } else {
+          // last item
+          focusTarget = tabItems[tabItems.length - 1]
+        }
+        break
+      case 'ArrowDown':
+        if (tabItems[index + 1]) {
+          // next item
+          focusTarget = tabItems[index + 1]
+        } else {
+          // first item
+          focusTarget = tabItems[0]
+        }
+        break
+      case 'Enter':
+        // Prevent default instead it will do Enter on the next node
+        e.preventDefault()
+        e.target.click()
+        break
+    }
+    if (focusTarget) {
+      focusTarget.focus()
+    }
   }
 }
-
 window.customElements.define('ss-dropdown', SSDropdown)
--- src/components/ss-results.js
+++ src/components/ss-results.js
@@ -1,6 +1,9 @@
 import { LitElement, html, css } from 'lit-element'
 import { GlobalStyles } from '../styles/global-styles'
 
+import { getPlaylists } from '../helpers/spotify'
+import { success } from '../components/ss-icons'
+
 import './ss-result-card'
 import './ss-dropdown'
 
@@ -14,7 +17,11 @@ export class SSResults extends LitElement {
     return {
       localTracks: { type: Array },
       selectedTracks: { type: Array },
-      spotifyResults: { type: Array }
+      spotifyResults: { type: Array },
+      playlists: { type: Array },
+      opened: { type: Boolean, reflect: true },
+      _selectedIndex: { type: Number },
+      _selectedPlaylist: { type: Object }
     }
   }
 
@@ -49,6 +56,72 @@ export class SSResults extends LitElement {
           color: var(--app-light-text);
           padding-right: 8px;
         }
+
+        .wrapper {
+          display: grid;
+          font-family: Roboto, sans-serif;
+          grid-template-columns: auto max-content;
+          grid-template-rows: 12px 16px;
+          grid-template-areas:
+              "label label"
+              "title arrow";
+          align-items: center;
+          gap: 6px 0;
+          padding: 4px 8px;
+          height: 40px;
+          box-sizing: border-box;
+          cursor: pointer;
+          outline: none;
+          width: 100%;
+          border-radius: 3px;
+          transition: 100ms background-color ease;
+        }
+        .wrapper p {
+          font-size: 11px;
+          margin: 0;
+          grid-area: label;
+          color: var(--app-neutral-400);
+        }
+        .wrapper h2 {
+          color: var(--app-light-text-color);
+          white-space: nowrap;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          font-size: 14px;
+          font-weight: bold;
+          text-transform: uppercase;
+          margin: 0;
+          grid-area: title;
+        }
+        .wrapper svg {
+          transform: rotate(0deg);
+          transition: transform .2s ease;
+          will-change: transform;
+          grid-area: arrow;
+        }
+        .wrapper:focus, .wrapper:hover {
+          background-color: var(--app-header-light-color);
+          transition: background-color 100ms;
+        }
+        hc-menu[opened] .wrapper svg {
+          transform: rotate(180deg);
+        }
+        *[slot="item"] {
+          transition: none;
+          width: 100%;
+        }
+        button[slot="item"] svg {
+          margin-right: 8px;
+          height: auto;
+          width: 20px;
+          fill: var(--app-neutral-1000);
+        }
+        button[slot="item"][selected] svg {
+          fill: var(--app-neutral-0);
+        }
+        ss-dropdown[opened] .wrapper svg:nth-of-type(2) {
+          transform: rotate(180deg);
+        }
       `
     ]
   }
@@ -59,12 +132,37 @@ export class SSResults extends LitElement {
     this.resultInnacuracyThreshold = 0.2
     this.selectedTracks = []
     this.spotifyResults = []
+    this.playlists = [1, 2, 3, 4, 5]
+    this._selectedPlaylist = {}
   }
 
   render () {
     return html`
       <div id="buttonCon">
-        <ss-dropdown></ss-dropdown>
+
+        <ss-dropdown @opened-changed=${this._openedChanged}>
+          <slot>
+            <div
+             class="wrapper"
+             tabindex="0">
+              ${success}
+              <h2>${this._selectedPlaylist}</h2>
+              ${success}
+            </div>
+          </slot>
+          <p slot="item">Playlist</p>
+          ${this.playlists.map(playlist => html`
+            <button
+              slot="item"
+              ?selected="${playlist === this._selectedPlaylist}"
+              tabindex="${this.opened ? '0' : '-1'}"
+              @click="${() => this._onMenuItemClicked(playlist)}">
+              ${success}
+              ${playlist}
+            </button>
+          `)}
+        </ss-dropdown>
+        
         <ss-button
           .label=${'Add to Spotify'}
           .icon=${'library_add'}
@@ -117,6 +215,14 @@ export class SSResults extends LitElement {
   _updateSelected () {
     const selected = this.spotifyResults.filter(result => result.selected === true)
     this.selectedTracks = selected
+  }
+
+  _onMenuItemClicked (index) {
+    this.selectedIndex = index
+  }
+
+  _openedChanged (e) {
+    this.opened = e.detail
   }
 }
 
